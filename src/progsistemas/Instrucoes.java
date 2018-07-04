a7d22013b11205ff907e881eacbff12aaf468645
cineasthales <thalesccastro@gmail.com>
2018-07-04 09:30:13 -0300

    Criação classes Principal e Instrucoes (com métodos definidos)

---
+++ src/progsistemas/Instrucoes.java
@@ -0,0 +1,259 @@
+package progsistemas;
+
+// Obs: instruções com '!!!' são as mais comuns, começar por elas! ///
+
+public class Instrucoes {
+
+    public void abcd() {
+    }
+
+    public void add() { // !!! ///
+    }
+
+    public void adda() {
+    }
+
+    public void addi() { // !!! ///
+    }
+
+    public void addq() {
+    }
+
+    public void addx() {
+    }
+
+    public void and() { // !!! ///
+    }
+
+    public void andi() { // !!! ///
+    }
+
+    public void anditoccr() {
+    }
+
+    public void anditosr() {
+    }
+
+    public void asl() {
+    }
+
+    public void asr() {
+    }
+
+    public void bcc() {
+    }
+
+    public void bchg() {
+    }
+
+    public void bclr() {
+    }
+
+    public void bra() { // !!! ///
+    }
+
+    public void bset() {
+    }
+
+    public void bsr() {
+    }
+
+    public void btst() {
+    }
+
+    public void chk() {
+    }
+
+    public void clr() {
+    }
+
+    public void cmp() { // !!! ///
+    }
+
+    public void cmpa() {
+    }
+
+    public void cmpi() { // !!! ///
+    }
+
+    public void cmpm() {
+    }
+
+    public void dbcc() {
+    }
+
+    public void divs() { // !!! ///
+    }
+
+    public void divu() { // !!! ///
+    }
+
+    public void eor() {
+    }
+
+    public void eori() {
+    }
+
+    public void eoritoccr() {
+    }
+
+    public void eoritosr() {
+    }
+
+    public void exg() {
+    }
+
+    public void ext() {
+    }
+
+    public void illegal() {
+    }
+
+    public void jmp() { // !!! ///
+    }
+
+    public void jsr() {
+    }
+
+    public void lea() {
+    }
+
+    public void link() { // !!! ///
+    }
+
+    public void lsl() { // !!! ///
+    }
+
+    public void lsr() { // !!! ///
+    }
+
+    public void move() { // !!! ///
+    }
+
+    public void movea() {
+    }
+
+    public void movetoccr() {
+    }
+
+    public void movefromsr() {
+    }
+
+    public void movetosr() {
+    }
+
+    public void moveusp() {
+    }
+
+    public void movem() {
+    }
+
+    public void movep() {
+    }
+
+    public void moveq() {
+    }
+
+    public void muls() { // !!! ///
+    }
+
+    public void mulu() { // !!! ///
+    }
+
+    public void nbcd() {
+    }
+
+    public void neg() { // !!! ///
+    }
+
+    public void negx() {
+    }
+
+    public void nop() { // !!! ///
+    }
+
+    public void not() { // !!! ///
+    }
+
+    public void or() { // !!! ///
+    }
+
+    public void ori() { // !!! ///
+    }
+
+    public void oritoccr() {
+    }
+
+    public void oritosr() {
+    }
+
+    public void pea() {
+    }
+
+    public void reset() { // !!! ///
+    }
+
+    public void rol() {
+    }
+
+    public void ror() {
+    }
+
+    public void roxl() {
+    }
+
+    public void roxr() {
+    }
+
+    public void rte() {
+    }
+
+    public void rtr() {
+    }
+
+    public void rts() {
+    }
+
+    public void sbcd() {
+    }
+
+    public void scc() {
+    }
+
+    public void stop() { // !!! ///
+    }
+
+    public void sub() { // !!! ///
+    }
+
+    public void suba() {
+    }
+
+    public void subi() { // !!! ///
+    }
+
+    public void subq() {
+    }
+
+    public void subx() {
+    }
+
+    public void swap() { // !!! ///
+    }
+
+    public void tas() {
+    }
+
+    public void trap() {
+    }
+
+    public void trapv() {
+    }
+
+    public void tst() {
+    }
+
+    public void unlk() {
+    }
+
+}
---
+++ src/progsistemas/Principal.java
@@ -0,0 +1,9 @@
+package progsistemas;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
