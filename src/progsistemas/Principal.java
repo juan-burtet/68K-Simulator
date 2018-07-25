45074b18ca1f1b999c08c7b3684780320d76e6de
Leonardo Coelho <30408216+leoGCoelho@users.noreply.github.com>
2018-07-25 16:36:50 -0300

    Novos arquivos de interface e principal alterado

--- src/progsistemas/Principal.java
+++ src/progsistemas/Principal.java
@@ -1,25 +1,45 @@
 package progsistemas;
 
-import java.io.*;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
+
 
 public class Principal {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-
-        // Mensagem inicial
-        JOptionPane.showMessageDialog(null, "Simulador do Motorola 68k v. 1.0", "Bem-vindo!", 1, null);
-
-        // Digita nome do arquivo de entrada
-        String arq = JOptionPane.showInputDialog(null, "Digite o nome do arquivo de entrada", "Arquivo", JOptionPane.QUESTION_MESSAGE);
-        while (arq == null || arq.equals("")) {
+    private String arq;
+    private String inArq;
+    private String outArq;
+    
+    public Principal() throws IOException{
+        arq = JOptionPane.showInputDialog(null, "Digite o nome do arquivo de entrada", "Arquivo", JOptionPane.QUESTION_MESSAGE);
+        while (/*arq != null || */ arq.equals("")) {
             arq = JOptionPane.showInputDialog(null, "Digite novamente o nome do arquivo de entrada", "Erro: nome inválido", JOptionPane.ERROR_MESSAGE);
         }
-
-        Memoria mem = new Memoria();
-        Montador m = new Montador();
-        m.monta(arq); 
-
+        
+        if(arq != null){
+            Montador mont = new Montador();
+            Memoria mem = new Memoria();
+            try {
+                mont.monta(arq);
+            } catch (IOException ex) {
+                Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            inArq = arq;
+            mont.monta(arq);
+            outArq = arq;
+        }
+        else{
+            arq = JOptionPane.showInputDialog(null, "Sem resultados obtidos", "Resultados", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    public String getInArq(){
+        return this.inArq;
+    }
+    public String getOutArq(){
+        return this.outArq;
     }
 
 }
